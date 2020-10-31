7d540d4432eedab2445916315482d5e355054d72
Xu jia quan <quannn0180716v@gmail.com>
2020-10-31 16:14:11 +0800

    設定 route 及對應的 Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('contact');
+    return view('welcome');
 });
+
+Route::get('index',[PostsController::class,'index'])->name('posts.index');
+
+Route::get('post',[PostsController::class,'post'])->name('posts.show');
+
+Route::get('about',[PostsController::class,'about'])->name('posts.about');
+
+Route::get('contact',[PostsController::class,'contact'])->name('posts.contact');
 
 
