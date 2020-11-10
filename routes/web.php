d1b56bd4929e3bd90383216881a16c81d970a68c
Xu jia quan <quannn0180716v@gmail.com>
2020-11-10 22:04:53 +0800

    新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
+
+    /* protected$fillable=[
+        'title',
+        'content',
+        'is_feature'
+    ];*/
+
+    $post=new Post();
+    $post->title='testtitle';
+    $post->content='testcontent';
+    $post->save();
+
 });
 
 Route::get('index',[PostsController::class,'index'])->name('posts.index');
