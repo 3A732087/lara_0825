7ea7de270037c861d3b0b31bd6b106e8f9e1f363
Xu jia quan <quannn0180716v@gmail.com>
2020-11-15 19:12:35 +0800

    Comment.php增加關聯

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Comment extends Model
 {
@@ -11,4 +12,11 @@ class Comment extends Model
 
     protected $fillable=[
         'content'];
+
+
+public function post()
+{
+    return $this->belongsTo(Post::class);
+}
+
 }
