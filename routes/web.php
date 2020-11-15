bb08bcd913158b5c2b5efd38e42c29db9974bf0d
Xu jia quan <quannn0180716v@gmail.com>
2020-11-15 19:22:21 +0800

    透過關聯將資料印出來

--- routes/web.php
+++ routes/web.php
@@ -67,8 +67,14 @@ Route::get('/', function () {
     /*$fourthPost=Post::find(4);
     dd($fourthPost);*/
 
-    $lastPost=Post::orderBy('id','DESC')->first();
-dd($lastPost);
+    /*$lastPost=Post::orderBy('id','DESC')->first();
+dd($lastPost);*/
+
+    $post = Post::find(8);
+    foreach($post->comments as $comment){
+        echo $comment->content.'<br>';
+    }
+
 
 
 
